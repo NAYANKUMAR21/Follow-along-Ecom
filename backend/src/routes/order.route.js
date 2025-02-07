0dc33c97f32c0318d180b2cd556b3186e26c9138
Nayan Kumar <naayaankumar@gmail.com>
2025-02-07 15:00:27 +0530

    M28 Done

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -4,9 +4,12 @@ const verifyUser = require('../middlewares/jwt-verify.js');
 const {
   GetUserOrdersController,
   CreateOrderController,
+  CancelOrder,
 } = require('../controllers/order.controller.js');
 
 router.get('/user-orders-data', verifyUser, GetUserOrdersController);
 router.post('/confirm-order', verifyUser, CreateOrderController);
+
+router.patch('/cancel-order', verifyUser, CancelOrder);
 
 module.exports = router;
